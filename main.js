2f987dad44cec401c889ae8ed7c8b48aef9794ee
Patryk Jary <49764884+K9xxxx@users.noreply.github.com>
2023-06-23 21:11:35 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ let tittleOfSection=document.querySelectorAll('.tittle-of-section')
 
 
 
+const tlInitial=new TimelineMax();
 
 
-
-
+tlInitial.to('.tittle-of-section',0,{opacity:0})
 
 $(document).ready(function(){
     let controller=new ScrollMagic.Controller();
-    
     let navScene=new ScrollMagic.Scene({
         triggerElement:'.header',
         triggerHook:'0',
@@ -22,13 +21,18 @@ $(document).ready(function(){
     .setClassToggle('.desk-nav','active-nav')
     .addTo(controller);
 
-    let tittleOfSectionScene=new ScrollMagic.Scene({
-        triggerElement: this,
-        offset:1,
-        reverse:false
+
+    $(tittleOfSection).each(function(){
+
+        let tittleOfSectionScene=new ScrollMagic.Scene({
+            triggerElement: this,
+            triggerHook:'0.88',
+            offset:1,
+            reverse:false
+        })
+        .addIndicators()
+        .setClassToggle(this,'active-tittle')
+        .addTo(controller);
     })
-    .addIndicators()
-    .setClassToggle(this,'active-tittle')
-    .addTo(controller);
 
 });
